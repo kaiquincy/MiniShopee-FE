953b21ecef4e499ceec14ccae004c9fb48f8cafd
kaiquincy <kawdwk@gmail.com>
2025-10-08 09:35:16 +0700

    fix somes bug

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { Box, Container, Flex, Text } from '@chakra-ui/react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from './Header'
 
 export default function Layout() {
