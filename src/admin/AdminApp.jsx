fa46285ad31dbd83dc14a00544e6ebcf0ca240fb
kaiquincy <kawdwk@gmail.com>
2025-12-18 05:57:20 +0700

    fix: Remove unused AdminOrders route from AdminApp

--- src/admin/AdminApp.jsx
+++ src/admin/AdminApp.jsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import AdminLayout from './components/AdminLayout'
 import AdminDashboard from './pages/AdminDashboard'
-import AdminOrders from './pages/AdminOrders'
 import AdminUsers from './pages/AdminUsers'
 import AdminProducts from './pages/AdminProducts'
 import AdminSettings from './pages/AdminSettings'
@@ -18,7 +17,6 @@ export default function AdminApp() {
       <Route element={<AdminLayout />}>
         <Route index element={<Navigate to="dashboard" replace />} />
         <Route path="dashboard" element={<AdminDashboard />} />
-        <Route path="orders" element={<AdminOrders />} />
         <Route path="users" element={<AdminUsers />} />
         <Route path="products" element={<AdminProducts />} />
         <Route path="settings" element={<AdminSettings />} />
